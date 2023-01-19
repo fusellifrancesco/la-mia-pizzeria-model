2e8bd034d5f893a40768c214b69ab033b980b1d7
fusellifrancesco <fusellifrancesco05@gmail.com>
2023-01-19 22:35:18 +0100

    Modifiche a LoremIpsum e dimensione immagine picsum su PizzaData

--- La mia pizzeria Model/Utils/PizzaData.cs
+++ La mia pizzeria Model/Utils/PizzaData.cs
@@ -15,7 +15,7 @@ namespace La_mia_pizzeria_Model.Utils {
             Pizze = new List<Pizza>();
             
             for (int i = 0; i < 10; i++) {
-                Pizza postGenerato = new Pizza(i, "Titolo post: " + i, "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", "https://picsum.photos/id/" + i + "/300");
+                Pizza postGenerato = new Pizza(i, "Pizza " + i, "Lorem Ipsum of Lorem Ipsum.", "https://picsum.photos/id/" + i + "/150");
                 Pizze.Add(postGenerato);
             }
             
