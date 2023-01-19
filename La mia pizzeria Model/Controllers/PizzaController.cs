1cad91967260fc42ad0c2fed99eb50bdbdb5ce9d
fusellifrancesco <fusellifrancesco05@gmail.com>
2023-01-19 22:30:54 +0100

    Completato PizzaController

--- La mia pizzeria Model/Controllers/PizzaController.cs
+++ La mia pizzeria Model/Controllers/PizzaController.cs
@@ -1,9 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NetCore_01.Models;
+using La_mia_pizzeria_Model.Utils;
 
 namespace La_mia_pizzeria_Model.Controllers {
     public class PizzaController : Controller {
+        
         public IActionResult Index() {
-            return View();
+
+            List<Pizza> ListaPizze = PizzaData.GetPizze();
+            
+            return View("Index", ListaPizze);
+        }
+
+        public IActionResult Details(int id) {
+            List<Pizza> ListaPizze = PizzaData.GetPizze();
+
+            foreach (Pizza pizze in ListaPizze) {
+                if (pizze.Id == id) {
+                    return View(pizze);
+                }
+            }
+
+            return NotFound("La pizza con l'id cercato non esiste!");
         }
     }
 }
