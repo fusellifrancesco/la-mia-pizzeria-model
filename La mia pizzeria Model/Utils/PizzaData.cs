1d851efd693469d926ca5f2a52e90dc5f069f737
fusellifrancesco <fusellifrancesco05@gmail.com>
2023-01-19 21:29:48 +0100

    Creato e completato PizzaData

---
+++ La mia pizzeria Model/Utils/PizzaData.cs
@@ -0,0 +1,26 @@
+﻿using NetCore_01.Models;
+
+namespace La_mia_pizzeria_Model.Utils {
+
+    public static class PizzaData {
+        
+        private static List<Pizza> Pizze;
+
+        public static List<Pizza> GetPizze() {
+
+            if (Pizze != null) {
+                return Pizze;
+            }
+
+            Pizze = new List<Pizza>();
+            
+            for (int i = 0; i < 10; i++) {
+                Pizza postGenerato = new Pizza(i, "Titolo post: " + i, "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", "https://picsum.photos/id/" + i + "/300");
+                Pizze.Add(postGenerato);
+            }
+            
+            return Pizze;
+        }
+    }
+}
+
